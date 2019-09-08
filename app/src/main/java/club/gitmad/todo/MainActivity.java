f9c14584d590dd030c568ba531893cd56627db47
FUAD\fuady <fuadmyoussef@gmail.com>
2019-09-08 03:26:27 -0500

    user can add assignment

--- app/src/main/java/club/gitmad/todo/MainActivity.java
+++ app/src/main/java/club/gitmad/todo/MainActivity.java
@@ -1,13 +1,18 @@
 package club.gitmad.todo;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,10 +28,33 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView textView = findViewById(R.id.Hello_WorldTV);
-                textView.setText("Button Pressed");
+                addItem();
             }
         });
+    }
+
+    private void addItem() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(R.id.dialog_box_label);
+        final View customLayout = getLayoutInflater().inflate(R.layout.alert_dialog_box, null);
+        builder.setView(customLayout);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText editText = customLayout.findViewById(R.id.taskNameEditText);
+                String taskName = editText.getText().toString();
+                TextView mainScreenTaskName = findViewById(R.id.task_name);
+                mainScreenTaskName.setText(taskName);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 
 }

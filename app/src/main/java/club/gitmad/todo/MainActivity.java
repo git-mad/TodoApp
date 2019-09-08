04f3806e054b75d937da6c61f0146cd0c62b2d1b
FUAD\fuady <fuadmyoussef@gmail.com>
2019-09-08 02:37:15 -0500

    Added FAB

--- app/src/main/java/club/gitmad/todo/MainActivity.java
+++ app/src/main/java/club/gitmad/todo/MainActivity.java
@@ -1,8 +1,14 @@
 package club.gitmad.todo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.Hello_WorldTV);
+                textView.setText("Button Pressed");
+            }
+        });
     }
+
 }

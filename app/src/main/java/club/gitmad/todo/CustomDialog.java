465b8d9704a0d2332fb41f20cef320775d84e95d
FUAD\fuady <fuadmyoussef@gmail.com>
2019-09-16 00:49:11 -0500

    removed alertdialog used dialog fragment instead

---
+++ app/src/main/java/club/gitmad/todo/CustomDialog.java
@@ -0,0 +1,56 @@
+package club.gitmad.todo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class CustomDialog extends DialogFragment {
+
+    private EditText input;
+    private TextView createButton, cancelButton;
+    public interface OnInputListener {
+        void sendInput(String input);
+    }
+    public OnInputListener mOnInputListener;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.alert_dialog_box, container, false);
+        input = view.findViewById(R.id.taskNameEditText);
+        createButton = view.findViewById(R.id.create_button);
+        cancelButton = view.findViewById(R.id.cancel_button);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taskName = input.getText().toString();
+                mOnInputListener.sendInput(taskName);
+                getDialog().dismiss();
+            }
+        });
+        return view;
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mOnInputListener= (OnInputListener) getActivity();
+        } catch (ClassCastException e){
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/club/gitmad/todo/MainActivity.java
+++ app/src/main/java/club/gitmad/todo/MainActivity.java
@@ -1,29 +1,23 @@
 package club.gitmad.todo;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.text.InputType;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements CustomDialog.OnInputListener {
+    private TextView mainScreenTaskName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mainScreenTaskName = findViewById(R.id.task_name);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,27 +28,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addItem() {
-
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(R.id.dialog_box_label);
-        final View customLayout = getLayoutInflater().inflate(R.layout.alert_dialog_box, null);
-        builder.setView(customLayout);
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                EditText editText = customLayout.findViewById(R.id.taskNameEditText);
-                String taskName = editText.getText().toString();
-                TextView mainScreenTaskName = findViewById(R.id.task_name);
-                mainScreenTaskName.setText(taskName);
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        builder.show();
+        CustomDialog dialog = new CustomDialog();
+        dialog.show(getSupportFragmentManager(), "CustomDialog");
     }
 
+    @Override
+    public void sendInput(String input) {
+        mainScreenTaskName.setText(input);
+    }
 }

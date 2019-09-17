bdcff0865849bf97c536fc656dce1300b5490f45
Jason Tu <title.kubo@gmail.com>
2019-09-16 20:09:29 -0400

    Finished lesson :)

--- app/src/main/java/club/gitmad/todo/MainActivity.java
+++ app/src/main/java/club/gitmad/todo/MainActivity.java
@@ -9,8 +9,8 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements CustomDialog.OnInputListener {
+    private TextView mainScreenTaskName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +18,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //Section 1 setting up the main screen:
-
+        mainScreenTaskName = findViewById(R.id.task_name);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
     }
+
+
     //Section 2 adding the item:
+    private void addItem() {
+        CustomDialog dialog = new CustomDialog();
+        dialog.show(getSupportFragmentManager(),"CustomDialog");
+    }
+
 
     //Section 3 receiving the data
+    @Override
+    public void sendInput(String input) {
+        mainScreenTaskName.setText(input);
+    }
 
 }
